9dd99dae9300af4093a1c94fc5c0b484ca49836f
yamchinsky <66372922+yamchinsky@users.noreply.github.com>
2021-06-24 22:06:48 +0300

    changes

--- src/views/movies/MoviesPage.js
+++ src/views/movies/MoviesPage.js
@@ -1,12 +1,53 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class MoviesPage extends Component {
-  state = {};
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+  };
+
+  state = {
+    name: "",
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state.name.trim());
+    if (!this.state.name) {
+      alert("Поле не может быть пустым!");
+      return;
+    }
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: "" });
+  };
 
   render() {
     return (
       <div>
-        <input type="text" />
+        <form className="SearchForm" onSubmit={this.handleSubmit}>
+          <input
+            className="SearchForm-input"
+            name="name"
+            onChange={this.handleChange}
+            value={this.state.name}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search your movie here"
+          />
+          <button type="submit" className="SearchForm-button">
+            <span className="SearchForm-button-label">Search</span>
+          </button>
+        </form>
       </div>
     );
   }
